177af58dd17d3385fee5f0de4943e7f016f91740
ryan-barrett <ryan.barrett@spookyhouse.org>
2017-10-22 22:47:36 -0700

    adding splash image

--- src/body/BodyContainer.js
+++ src/body/BodyContainer.js
@@ -1,110 +1,116 @@
-import React, {Component} from 'react'
-import Carousel from './Carousel.js'
-import TopicList from './TopicList.js'
-import CityListAndShowcase from './CityListAndShowcase.js'
-import $ from 'jquery-ajax'
+import React, { Component } from "react";
+import Carousel from "./Carousel.js";
+import TopicList from "./TopicList.js";
+import CityListAndShowcase from "./CityListAndShowcase.js";
+import $ from "jquery-ajax";
+import "../Home.css";
 
 class BodyContainer extends Component {
-
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       cities: [],
       selectedCityObj: {},
       selectedPosts: []
-    }
+    };
   }
 
   componentWillMount = () => {
     $.ajax({
-      method: 'GET',
-      url: 'https://wayfar.herokuapp.com/api/cities/'
-    })
-    .then((res) => {
-      this.setState(
-        {
+      method: "GET",
+      url: "https://wayfar.herokuapp.com/api/cities/"
+    }).then(
+      res => {
+        this.setState({
           cities: res,
           selectedCityObj: res[0]
-        }
-      )
-      this.loadPostsFromServer()
-    }, (err) => {
-      console.log('error: ', err)
-    })
-  }
+        });
+        this.loadPostsFromServer();
+      },
+      err => {
+        console.log("error: ", err);
+      }
+    );
+  };
 
   loadCitiesFromServer = () => {
     $.ajax({
-      method: 'GET',
-      url: 'https://wayfar.herokuapp.com/api/cities/'
-    })
-    .then((res) => {
-      this.setState(
-        {
+      method: "GET",
+      url: "https://wayfar.herokuapp.com/api/cities/"
+    }).then(
+      res => {
+        this.setState({
           cities: res,
           selectedCityObj: res[0]
-        }
-      )
-      this.loadPostsFromServer()
-    }, (err) => {
-      console.log('error: ', err)
-    })
-  }
+        });
+        this.loadPostsFromServer();
+      },
+      err => {
+        console.log("error: ", err);
+      }
+    );
+  };
 
   loadPostsFromServer = () => {
     $.ajax({
-      method: 'GET',
-      url: 'https://wayfar.herokuapp.com/api/cities/' + this.state.selectedCityObj._id + '/posts'
-    })
-    .then(res => {
-      this.setState({ selectedPosts: res.postsByCity })
-    })
-  }
+      method: "GET",
+      url:
+        "https://wayfar.herokuapp.com/api/cities/" +
+        this.state.selectedCityObj._id +
+        "/posts"
+    }).then(res => {
+      this.setState({ selectedPosts: res.postsByCity });
+    });
+  };
 
   componentDidMount = () => {
-    this.loadCitiesFromServer()
-  }
+    this.loadCitiesFromServer();
+  };
 
-  handleCitySelect = (event) => {
-    event.preventDefault()
-    let cityId = $(event.target).closest('.click-for-city').data('city-id')
+  handleCitySelect = event => {
+    event.preventDefault();
+    let cityId = $(event.target)
+      .closest(".click-for-city")
+      .data("city-id");
 
-    let allCities = this.state.cities
+    let allCities = this.state.cities;
 
-    let newSelectedCityObj = allCities.filter(function (city) {
-      return (city._id === cityId)
-    })
+    let newSelectedCityObj = allCities.filter(function(city) {
+      return city._id === cityId;
+    });
 
-    this.setState({ selectedCityObj: newSelectedCityObj[0] })
+    this.setState({ selectedCityObj: newSelectedCityObj[0] });
 
     $.ajax({
-      method: 'GET',
-      url: 'https://wayfar.herokuapp.com/api/cities/' + cityId + '/posts'
-    })
-    .then(res => {
-      this.setState({ selectedPosts: res.postsByCity })
-    })
-  }
+      method: "GET",
+      url: "https://wayfar.herokuapp.com/api/cities/" + cityId + "/posts"
+    }).then(res => {
+      this.setState({ selectedPosts: res.postsByCity });
+    });
+  };
 
-  render () {
+  render() {
     if (!this.props.isLoggedIn) {
       return (
         <div>
-          <Carousel cities={this.state.cities} handleCitySelect={(event)=>this.handleCitySelect(event)} />
-          <TopicList cities={this.state.cities} handleCitySelect={(event)=>this.handleCitySelect(event)} />
+          <div className="splash container-fluid" />
+          <TopicList
+            cities={this.state.cities}
+            handleCitySelect={event => this.handleCitySelect(event)}
+          />
         </div>
-      )
+      );
     }
     return (
       <CityListAndShowcase
         cities={this.state.cities}
         selectedCityObj={this.state.selectedCityObj}
         selectedPosts={this.state.selectedPosts}
-        handleCitySelect={(event)=>this.handleCitySelect(event)}
-        loadPostsFromServer={(event)=>this.loadPostsFromServer(event)}
-        />
-    )
+        handleCitySelect={event => this.handleCitySelect(event)}
+        loadPostsFromServer={event => this.loadPostsFromServer(event)}
+      />
+    );
   }
 }
 
-export default BodyContainer
+export default BodyContainer;
--- src/body/Carousel.js
+++ src/body/Carousel.js
@@ -1,53 +1,60 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
+import "../Home.css";
 
 class Carousel extends Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       cities: [
         {
-          _id: '59a59993ae49b000075d005e',
-          name: 'San Francisco',
-          description: 'San Francisco was founded on June 29, 1776, when colonists from Spain established Presidio of San Francisco at the Golden Gate and Mission San Francisco de Asís a few miles away, all named for St. Francis of Assisi. The California Gold Rush of 1849 brought rapid growth, making it the largest city on the West Coast at the time.',
-          img_url: 'https://image.ibb.co/iRiSok/sf.jpg',
+          _id: "59a59993ae49b000075d005e",
+          name: "San Francisco",
+          description:
+            "San Francisco was founded on June 29, 1776, when colonists from Spain established Presidio of San Francisco at the Golden Gate and Mission San Francisco de Asís a few miles away, all named for St. Francis of Assisi. The California Gold Rush of 1849 brought rapid growth, making it the largest city on the West Coast at the time.",
+          img_url: "https://image.ibb.co/iRiSok/sf.jpg",
           __v: 0
         },
         {
-          _id: '59a59993ae49b000075d005f',
-          name: 'London',
-          description: 'London is the capital of the United Kingdom, a country in western Europe. It is also the capital of England, which is part of the United Kingdom. London lies on the Thames River. It is the largest city in the country.',
-          img_url: 'https://i.imgur.com/LziiBHl.jpg',
+          _id: "59a59993ae49b000075d005f",
+          name: "London",
+          description:
+            "London is the capital of the United Kingdom, a country in western Europe. It is also the capital of England, which is part of the United Kingdom. London lies on the Thames River. It is the largest city in the country.",
+          img_url: "https://i.imgur.com/LziiBHl.jpg",
           __v: 0
         },
         {
-          _id: '59a59993ae49b000075d0060',
-          name: 'Sydney',
-          description: 'Sydney, capital of New South Wales and one of Australias largest cities, is best known for its harbourfront Sydney Opera House, with a distinctive sail-like design. Massive Darling Harbour and the smaller Circular Quay port are hubs of waterside life, with the arched Harbour Bridge and esteemed Royal Botanic Garden nearby.',
-          img_url: 'https://image.ibb.co/g2up15/aussie.jpg',
+          _id: "59a59993ae49b000075d0060",
+          name: "Sydney",
+          description:
+            "Sydney, capital of New South Wales and one of Australias largest cities, is best known for its harbourfront Sydney Opera House, with a distinctive sail-like design. Massive Darling Harbour and the smaller Circular Quay port are hubs of waterside life, with the arched Harbour Bridge and esteemed Royal Botanic Garden nearby.",
+          img_url: "https://image.ibb.co/g2up15/aussie.jpg",
           __v: 0
         }
       ]
-    }
+    };
   }
 
-  render () {
+  render() {
     let citiesImages = this.state.cities.map(city => {
       return (
-        <a key={city._id} className='carousel-item click-for-city' data-city-id={city._id}>
-          <img src={city.img_url} alt='' key={city._id} />
+        <a
+          key={city._id}
+          className="carousel-item click-for-city"
+          data-city-id={city._id}
+        >
+          <img src={city.img_url} alt="" key={city._id} />
         </a>
-      )
-    })
+      );
+    });
     return (
-      <div className='col s12 carousel-holder'>
-        <div className='carousel carousel-slider'>
-          { citiesImages }
-        </div>
-        <i className='material-icons icon icon-left'>chevron_left</i>
-        <i className='material-icons icon icon-right'>chevron_right</i>
+      <div className="col s12 carousel-holder">
+        <div className="splash container-fluid" />
+        <div className="carousel carousel-slider">{citiesImages}</div>
+        <i className="material-icons icon icon-left">chevron_left</i>
+        <i className="material-icons icon icon-right">chevron_right</i>
       </div>
-    )
+    );
   }
 }
 
-export default Carousel
+export default Carousel;
