bce273c905dbe11e7a08bfb87b502c3b7bc8e4af
ryan-barrett <ryan.barrett@spookyhouse.org>
2017-11-02 13:03:15 -0700

    fixed api urls to work with heroku

--- src/App.js
+++ src/App.js
@@ -1,106 +1,128 @@
-import React, { Component } from 'react'
-import BodyContainer from './body/BodyContainer.js'
-import Header from './head/Header.js'
-import $ from 'jquery-ajax'
-import './Home.css'
+import React, { Component } from "react";
+import BodyContainer from "./body/BodyContainer.js";
+import Header from "./head/Header.js";
+import $ from "jquery-ajax";
+import "./Home.css";
 
 class App extends Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      signUpUserName: '',
-      signUpFirstName: '',
-      signUpLastName: '',
-      signUpPassword: '',
-      userName: '',
-      password: '',
-      userId: '',
+      signUpUserName: "",
+      signUpFirstName: "",
+      signUpLastName: "",
+      signUpPassword: "",
+      userName: "",
+      password: "",
+      userId: "",
       isLoggedIn: false,
       isSignInOpen: false,
-      isSignUpOpen: false,
-    }
+      isSignUpOpen: false
+    };
   }
 
-  handleChange = (event) => {
-    let userInfo = $(event.target).closest('.validate').data('id-type');
-    this.setState({[userInfo]: event.target.value})
-  }
+  handleChange = event => {
+    let userInfo = $(event.target)
+      .closest(".validate")
+      .data("id-type");
+    this.setState({ [userInfo]: event.target.value });
+  };
 
   toggleSignupModal = () => {
-    this.setState({isSignUpOpen: !this.state.isSignUpOpen})
-  }
+    this.setState({ isSignUpOpen: !this.state.isSignUpOpen });
+  };
 
-  handleSignupSubmit = (event) => {
-    event.preventDefault()
+  handleSignupSubmit = event => {
+    event.preventDefault();
     $.ajax({
-      method: 'POST',
-      url: 'https://wayfar.herokuapp.com/signup',
+      method: "POST",
+      url: "https://nomad-.herokuapp.com/signup",
       data: {
         first_name: this.state.signUpFirstName,
         last_name: this.state.signUpLastName,
         password: this.state.signUpPassword,
         username: this.state.signUpUserName
       }
-    })
-    .then((res) => {
-      console.log(res)
-      this.toggleSignupModal()
-    },
-    (err) => {
-      alert('User already exists')
-    })
-  }
+    }).then(
+      res => {
+        console.log(res);
+        this.toggleSignupModal();
+      },
+      err => {
+        alert("User already exists");
+      }
+    );
+  };
 
   toggleSignInModal = () => {
-    this.setState({isSignInOpen: !this.state.isSignInOpen})
-  }
+    this.setState({ isSignInOpen: !this.state.isSignInOpen });
+  };
 
-  handleUserNameChange = (event) => {
-    this.setState({userName: event.target.value})
-  }
+  handleUserNameChange = event => {
+    this.setState({ userName: event.target.value });
+  };
 
-  handlePasswordChange = (event) => {
-    this.setState({password: event.target.value})
-  }
+  handlePasswordChange = event => {
+    this.setState({ password: event.target.value });
+  };
 
-  handleSignInSubmit = (event) => {
-    event.preventDefault()
+  handleSignInSubmit = event => {
+    event.preventDefault();
     $.ajax({
-      method: 'POST',
-      url: 'https://wayfar.herokuapp.com/login',
+      method: "POST",
+      url: "https://nomad-.herokuapp.com/login",
       data: {
         username: this.state.userName,
         password: this.state.password
       }
-    })
-    .then((res) => {
-      console.log(res, 'User is authenticated')
-      this.setState({userId: res._id, isLoggedIn: true})
-      this.toggleSignInModal()
-    },
-    (err) => {
-      alert('Your Credentials Are Incorrect')
-      this.setState({
-        userName: '',
-        password: '',
-        userId: '',
-        isLoggedIn: false
-      })
-    })
-  }
+    }).then(
+      res => {
+        console.log(res, "User is authenticated");
+        this.setState({ userId: res._id, isLoggedIn: true });
+        this.toggleSignInModal();
+      },
+      err => {
+        alert("Your Credentials Are Incorrect");
+        this.setState({
+          userName: "",
+          password: "",
+          userId: "",
+          isLoggedIn: false
+        });
+      }
+    );
+  };
 
-  handleLogOut = (event) => {
-    this.setState({isLoggedIn:false})
-  }
+  handleLogOut = event => {
+    this.setState({ isLoggedIn: false });
+  };
 
-  render () {
+  render() {
     return (
-      <div className='home'>
-        <Header handleChange={(event) => this.handleChange(event)} toggleSignInModal={(event) => this.toggleSignInModal(event)} toggleSignupModal={(event)=> this.toggleSignupModal(event)} handleSignupSubmit={(event) => this.handleSignupSubmit(event)} handleUserNameChange={(event) => this.handleUserNameChange(event)} handlePasswordChange={(event) => this.handlePasswordChange(event)} handleSignInSubmit={(event) => this.handleSignInSubmit(event)} handleLogOut={(event) => this.handleLogOut(event)} userId={this.state.userId} userName={this.state.userName} isLoggedIn={this.state.isLoggedIn} isSignInOpen={this.state.isSignInOpen} isSignUpOpen={this.state.isSignUpOpen}/>
-        <BodyContainer userId={this.state.userId} isLoggedIn={this.state.isLoggedIn} handleChange={(event => this.handleChange(event))} />
+      <div className="home">
+        <Header
+          handleChange={event => this.handleChange(event)}
+          toggleSignInModal={event => this.toggleSignInModal(event)}
+          toggleSignupModal={event => this.toggleSignupModal(event)}
+          handleSignupSubmit={event => this.handleSignupSubmit(event)}
+          handleUserNameChange={event => this.handleUserNameChange(event)}
+          handlePasswordChange={event => this.handlePasswordChange(event)}
+          handleSignInSubmit={event => this.handleSignInSubmit(event)}
+          handleLogOut={event => this.handleLogOut(event)}
+          userId={this.state.userId}
+          userName={this.state.userName}
+          isLoggedIn={this.state.isLoggedIn}
+          isSignInOpen={this.state.isSignInOpen}
+          isSignUpOpen={this.state.isSignUpOpen}
+        />
+        <BodyContainer
+          userId={this.state.userId}
+          isLoggedIn={this.state.isLoggedIn}
+          handleChange={event => this.handleChange(event)}
+        />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
